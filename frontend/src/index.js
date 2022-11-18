ab3ff48ef532200eb381db626ce58daa8902f0e5
syafiqfadillah <codingan77@gmail.com>
2022-11-18 09:57:07 +0700

    fix bug

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,9 +14,9 @@ import Cart from "./Pages/Cart"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/*<App />*/}
+    <App />
      {/* <Setting />  */}
-     <Toko/>
+     {/*<Toko/>*/}
      {/* <Cart/> */}
     {/*<DetailProduct />*/}
     {/*<Checkout />*/}
